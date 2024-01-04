2de5fd4f8ae3ddf77e84aaa4792faba9dee30368
rakib <hassanrakibul926@gmail.com>
2024-01-04 14:14:44 +0600

    add skill section title

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../About/About"
 import Banner from "../Banner/Banner"
+import Skill from "../Skill/Skill"
 
 const Home = () =>{
     return (
         <div>
             <Banner></Banner>
             <About></About>
+            <Skill></Skill>
         </div>
     )
 }
---
+++ src/Components/Pages/Skill/Skill.jsx
@@ -0,0 +1,14 @@
+import Title from "../../ShareComponent/NavBar/Title"
+
+const Skill = () =>{
+    return (
+        <div className="md:w-3/4 mx-auto py-8">
+            <Title title='Skills' summary='Technologies I Like To Work With'></Title>
+            <div className="w-2/4 mx-auto h-[75vh] border-t border-l border-r border-secondaryColor rounded-full mt-12">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Skill
